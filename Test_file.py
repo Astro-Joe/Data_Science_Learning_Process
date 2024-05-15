a53bc15120def93c03c9c43c29e53a7a5276ee87
Astro-Joe <ilemobayojoseph06@gmail.com>
2024-05-15 13:12:20 +0100

    Created a test file

---
+++ Test_file.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+#Previous outputs in python and also in jupyter notebook can be access using this shotcut syntax
+print(_)
+print(__)
+get_ipython().run_line_magic('history', '-n 27-70')
